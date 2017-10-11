11e9d653ca784fa831720a7b075a614df41e7e1b
Seiya Kokushi <xxx.xyz-s-.xxx@i.softbank.jp>
2017-10-11 23:23:17 +0900

    initial commit

---
+++ app/src/main/kotlin/com/ronnnnn/giphy_mvvm/App.kt
@@ -0,0 +1,15 @@
+package com.ronnnnn.giphy_mvvm
+
+import android.app.Application
+
+/**
+ * Created by kokushiseiya on 2017/10/11.
+ */
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+}
---
+++ app/src/main/kotlin/com/ronnnnn/giphy_mvvm/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ronnnnn.giphy_mvvm.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.ronnnnn.giphy_mvvm.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
