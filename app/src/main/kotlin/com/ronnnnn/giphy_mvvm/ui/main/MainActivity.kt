2a5d427dde80c7e5b311aac1257f8ec2c2e6b382
Seiya Kokushi <xxx.xyz-s-.xxx@i.softbank.jp>
2017-10-19 21:55:44 +0900

    fix fragment back action

--- app/src/main/kotlin/com/ronnnnn/giphy_mvvm/ui/main/MainActivity.kt
+++ app/src/main/kotlin/com/ronnnnn/giphy_mvvm/ui/main/MainActivity.kt
@@ -5,10 +5,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.ronnnnn.giphy_mvvm.R
 import com.ronnnnn.giphy_mvvm.databinding.ActivityMainBinding
-import com.ronnnnn.giphy_mvvm.ui.detail.DetailFragment
-import com.ronnnnn.giphy_mvvm.ui.trending.TrendingFragment
 
-class MainActivity : AppCompatActivity(), TrendingFragment.Listener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +21,11 @@ class MainActivity : AppCompatActivity(), TrendingFragment.Listener {
         }
     }
 
-    override fun onItemClicked(gifId: String) {
-        supportFragmentManager.beginTransaction()
-                .replace(R.id.container_frame_layout, DetailFragment.createInstance(gifId))
-                .addToBackStack(DetailFragment::class.java.name)
-                .commit()
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount < 2) {
+            finish()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
--- app/src/main/kotlin/com/ronnnnn/giphy_mvvm/ui/trending/TrendingFragment.kt
+++ app/src/main/kotlin/com/ronnnnn/giphy_mvvm/ui/trending/TrendingFragment.kt
@@ -1,6 +1,5 @@
 package com.ronnnnn.giphy_mvvm.ui.trending
 
-import android.content.Context
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -11,34 +10,23 @@ import android.view.ViewGroup
 import com.ronnnnn.giphy_mvvm.R
 import com.ronnnnn.giphy_mvvm.data.json.response.gif.Gif
 import com.ronnnnn.giphy_mvvm.databinding.FragmentTrendingBinding
+import com.ronnnnn.giphy_mvvm.ui.detail.DetailFragment
 
 /**
  * Created by kokushiseiya on 2017/10/13.
  */
-class TrendingFragment private constructor() : Fragment(), TrendingContract {
+class TrendingFragment private constructor() : Fragment(), TrendingContract, TrendingRecyclerAdapter.Listener {
 
     companion object {
 
-        fun createInstance(): Fragment =
-                TrendingFragment()
+        fun createInstance(): Fragment = TrendingFragment()
     }
 
-    private var listener: Listener? = null
     private var recyclerAdapter: TrendingRecyclerAdapter? = null
-
-    override fun onAttach(context: Context?) {
-        super.onAttach(context)
-
-        listener = activity as Listener
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
             DataBindingUtil.inflate<FragmentTrendingBinding>(inflater, R.layout.fragment_trending, container, false).run {
-                recyclerAdapter = TrendingRecyclerAdapter(context, object : TrendingRecyclerAdapter.Listener {
-                    override fun onItemClicked(gifId: String) {
-                        listener?.onItemClicked(gifId)
-                    }
-                })
+                recyclerAdapter = TrendingRecyclerAdapter(context, this@TrendingFragment)
                 trendingRecyclerView.run {
                     layoutManager = LinearLayoutManager(context)
                     adapter = recyclerAdapter
@@ -52,7 +40,11 @@ class TrendingFragment private constructor() : Fragment(), TrendingContract {
         recyclerAdapter?.setItemsAndNotify(images)
     }
 
-    interface Listener {
-        fun onItemClicked(gifId: String)
+    override fun onItemClicked(gifId: String) {
+        activity.supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container_frame_layout, DetailFragment.createInstance(gifId))
+                .addToBackStack(DetailFragment::class.java.name)
+                .commit()
     }
 }
